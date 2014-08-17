a1a28075b010aed10af690487dd23661ffdf9828
James Elmour <james.elmour@gmail.com>
2014-08-17 22:23:45 +0100

    Add delete button, refactored UI, added runs entry

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import time
+import os
 from splinter import Browser
 from tkinter import *
 
-class testconfig():
-    runs = 3
-    timeout = 5
-    totaltime = 0
-    loadtime = 0
-    loadstart = 0
-
-config = testconfig()
+def deletefile():
+    os.remove("results.csv")
 
 def writetoresults(towrite):
     with open("results.csv", "a") as results:
@@ -28,12 +23,12 @@ def testrunner(methods):
 
 def firefoxtest():
     with Browser() as browser:
-        config.totaltime = 0
+        runs = int(runentry.get())
         writetoresults("Firefox run on " + str(time.strftime("%d/%m/%Y")) + " at " + str(time.strftime("%H:%M:%S") + "\n"))
         i = 0
         f = 0
         timer = time.clock()
-        while i < config.runs:
+        while i < runs:
             runstart = time.clock()
             f = f + testrunner([
                 browser.visit("https://m.quote.comparethemarket.com/car/newquote/?AFFCLIE=CM01"),
@@ -46,19 +41,18 @@ def firefoxtest():
             writetoresults("Test run " + str(i) + " total time," + str(runend - runstart) + "\n")
             i = i + 1
         endtimer = time.clock()
-        writetoresults("Total time," + str(endtimer - timer) + "\n\n")
         totaltime = endtimer - timer
-        writetoresults("Total time," + str(endtimer - timer) + ",Average," + str(totaltime/config.runs) + "\n\n")
-        FFtimelabel.config(text = "This test ran " + str(config.runs) + " times Firefox completed at: " + str(totaltime) + " and failed " + str(f) + " times. Average time of " + str(totaltime/config.runs))
+        writetoresults("Total time," + str(endtimer - timer) + ",Average," + str(totaltime/runs) + "\n\n")
+        FFtimelabel.config(text = "This test ran " + str(runs) + " times Firefox completed at: " + str(totaltime) + " and failed " + str(f) + " times. Average time of " + str(totaltime/runs))
 
 def chrometest():
     with Browser("chrome") as browser:
-        config.totaltime = 0
+        runs = int(runentry.get())
         writetoresults("Chrome run on " + str(time.strftime("%d/%m/%Y")) + " at " + str(time.strftime("%H:%M:%S") + "\n"))
         i = 0
         f = 0
         timer = time.clock()
-        while i < config.runs:
+        while i < runs:
             runstart = time.clock()
             f = f + testrunner([
                 browser.visit("https://m.quote.comparethemarket.com/car/newquote/?AFFCLIE=CM01"),
@@ -72,8 +66,8 @@ def chrometest():
             i = i + 1
         endtimer = time.clock()
         totaltime = endtimer - timer
-        writetoresults("Total time," + str(endtimer - timer) + ",Average," + str(totaltime/config.runs) + "\n\n")
-        CHtimelabel.config(text = "This test ran " + str(config.runs) + " times, Chrome completed at " + str(totaltime) + " and failed " + str(f) + " times. Average time of " + str(totaltime/config.runs))
+        writetoresults("Total time," + str(endtimer - timer) + ",Average," + str(totaltime/runs) + "\n\n")
+        CHtimelabel.config(text = "This test ran " + str(runs) + " times, Chrome completed at " + str(totaltime) + " and failed " + str(f) + " times. Average time of " + str(totaltime/runs))
 
 def clearText():
     CHtimelabel.config(text = "")
@@ -83,12 +77,21 @@ def clearText():
 root = Tk()
 root.minsize(800,100)
 root.resizable(width=FALSE, height=FALSE)
-clear = Button(root, text="Clear", command=clearText)
-clear.pack()
-fftest = Button(root, text="Run in FireFox", command=firefoxtest)
-fftest.pack()
-chtest = Button(root, text="Run in Chrome", command=chrometest)
-chtest.pack()
+toolbar = Frame(root)
+clear = Button(toolbar, text="Delete results file", command=deletefile)
+clear.pack({"side": "left"})
+clear = Button(toolbar, text="Clear", command=clearText)
+clear.pack({"side": "left"})
+fftest = Button(toolbar, text="Run in FireFox", command=firefoxtest)
+fftest.pack({"side": "left"})
+chtest = Button(toolbar, text="Run in Chrome", command=chrometest)
+chtest.pack({"side": "left"})
+Label(toolbar, text="Runs: ").pack({"side": "left"})
+runentry = Entry(toolbar, width=3)
+runentry.insert(0, "3")
+runentry.pack({"side": "left"})
+toolbar.pack()
+
 FFtimelabel = Label(root)
 FFtimelabel.pack()
 CHtimelabel = Label(root)
